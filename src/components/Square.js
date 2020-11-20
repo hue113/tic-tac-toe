3ebed761e8ee64ef8011e1e180a0752ad23b5fb9
Hue Pham <huepham113@gmail.com>
2020-11-20 00:13:31 -0500

    use this.state.value in button in Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './Square.css';
 
 class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          value: null,
+        };
+    }
+
     render() {
         return (
             // learn to pass a prop from a parent Board component to a child Square component
-            <button className="square">
-                {this.props.value}
+            // change this.props.value --> this.state.value
+            <button className="square" onClick={ () => this.setState({value: 'X'}) }>
+                {this.state.value}
             </button>
         );
     }
