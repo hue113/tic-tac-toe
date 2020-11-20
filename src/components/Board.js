c643aa2f0926e9f3c015adbf6c01848e03c8eefb
Hue Pham <huepham113@gmail.com>
2020-11-20 18:42:50 -0500

    Add handleClick() to Square

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,9 +4,25 @@ import Square from './Square';
 
 
 class Board extends Component {
-    
+    // Add a constructor to the Board
+    // set the Board’s initial state to contain an array of 9 nulls corresponding to the 9 squares
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+        };
+    }
+
+    // In 02-state, X is determined by Square’s own state
+    // This is why Square ignores the value prop passed to it by the Board
+    // Now, we change value={i} to value={this.state.squares[i]}
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
