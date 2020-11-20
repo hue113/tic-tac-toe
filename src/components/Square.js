42ee38408592b4d2de0112b33ab86e2dced5b3d6
Hue Pham <huepham113@gmail.com>
2020-11-19 20:47:39 -0500

    Learn to pass a prop from a parent Board component to a child Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Square extends Component {
     render() {
         return (
+            // learn to pass a prop from a parent Board component to a child Square component
             <button className="square">
                 {this.props.value}
             </button>
