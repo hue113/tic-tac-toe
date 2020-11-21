d1772f97124bf2532a83861872c864a0bbf39866
Hue Pham <huepham113@gmail.com>
2020-11-21 00:07:07 -0500

    Change Square into a Function Component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import './Square.css';
 
-class Square extends Component {
-    //03-lift-state-up: Delete the constructor from Square
+// 04: change the Square to be a function component
+// Function components = Components that only have 1 render() and don't have their own state (no constructor) 
+// It takes (props) as input and returns what should be rendered
+// must change this.props --> props
+// Why using Function components, not Class? --> because it's less tedious
 
-    render() {
-        return (
-            // 01-props: learn to pass a prop from a parent Board component to a child Square component
-            // 02-state: change this.props.value --> this.state.value
-            // That means: Square maintains its state, Board only render <Square />; that's why we don't need to use props.value 
-            // 03-lift-state-up: Board pass 2 props to Square: value & onClick
-            // That's why we're back to use this.props.value & this.props.onClick to communicate with Board
-            // And we can Delete the constructor from Square
-            <button className="square" onClick={ () => this.props.onClick() }>
-                {this.props.value}
-            </button>
-        );
-    }
+function Square (props) {
+    return (
+        // this.props.value --> props.value
+        // this.props.onClick --> props.onClick
+        <button className="square" onClick={ () => props.onClick() }>
+            {props.value}
+        </button>
+    );
 }
 
 export default Square;
