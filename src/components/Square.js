6a483f1f474bf4405b80615dc8e99d5b96846a37
Hue Pham <huepham113@gmail.com>
2020-11-20 23:07:00 -0500

    Delete constructor, Change render() to use props in Square

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import './Square.css';
 
 class Square extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          value: null,
-        };
-    }
+    //03-lift-state-up: Delete the constructor from Square
 
     render() {
         return (
-            // learn to pass a prop from a parent Board component to a child Square component
-            // change this.props.value --> this.state.value
-            <button className="square" onClick={ () => this.setState({value: 'X'}) }>
-                {this.state.value}
+            // 01-props: learn to pass a prop from a parent Board component to a child Square component
+            // 02-state: change this.props.value --> this.state.value
+            // That means: Square maintains its state, Board only render <Square />; that's why we don't need to use props.value 
+            // 03-lift-state-up: Board pass 2 props to Square: value & onClick
+            // That's why we're back to use this.props.value & this.props.onClick to communicate with Board
+            // And we can Delete the constructor from Square
+            <button className="square" onClick={ () => this.props.onClick() }>
+                {this.props.value}
             </button>
         );
     }
