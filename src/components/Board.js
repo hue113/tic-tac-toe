e17d563ff2443d2c0ceb86cb299bbdf2a0dcd063
Hue Pham <huepham113@gmail.com>
2020-11-20 23:05:55 -0500

    Add constructor, handleClick(), change renderSquare() in Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,8 @@ import Square from './Square';
 
 
 class Board extends Component {
-    // Add a constructor to the Board
-    // set the Board’s initial state to contain an array of 9 nulls corresponding to the 9 squares
+    // 03-lift-state-up: Add a constructor to the Board
+    // set the Board’s initial state to contain an array of 9 nulls (corresponding to the 9 squares)
     constructor(props) {
         super(props);
         this.state = {
@@ -13,9 +13,16 @@ class Board extends Component {
         };
     }
 
+    // Add function handleClick
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     // In 02-state, X is determined by Square’s own state
     // This is why Square ignores the value prop passed to it by the Board
-    // Now, we change value={i} to value={this.state.squares[i]}
+    // Now, we use props again and pass down two props from Board to Square: value & onClick
     renderSquare(i) {
         return (
             <Square 
