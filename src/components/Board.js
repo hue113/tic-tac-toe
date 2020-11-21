6d0fbfe109c0f3afbdabf7a89a4504d2e4040044
Hue Pham <huepham113@gmail.com>
2020-11-21 17:35:35 -0500

    Add helper function calculateWinner to Board.js; update handleClick() & status in render() to determine winner or next turn

--- src/components/Board.js
+++ src/components/Board.js
@@ -19,10 +19,18 @@ class Board extends Component {
     // 05: Use boolean xIsNext to determine who goes next
     handleClick(i) {
         const squares = this.state.squares.slice();
+
+        // 06: Check winner before determining who goes next
+        // Note: Boolean (function ()) = true if function doesn't return null
+        // Boolean (square[i]) = true if square[i] is not null
+        if (calculateWinner(squares) || squares[i]) {       
+          return;
+        }
+
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
         });
     }
 
@@ -39,9 +47,17 @@ class Board extends Component {
     }
 
     render() {
-        // 05: change status to display which player has the next turn
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        
+        // 05: change status to display which player has the next turn        
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        // 06: Check winner, if not,  determine who goes next
+        // winner = true if calculateWinner(this.state.squares) NOT return null
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
         return (
             <div>
                 <div className="status">{status}</div>
@@ -63,6 +79,31 @@ class Board extends Component {
             </div>
         );
     }
+
+    
 }
+
+
+// helper function
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];      // X or O
+      }
+    }
+    return null;
+}
+  
 
 export default Board;
