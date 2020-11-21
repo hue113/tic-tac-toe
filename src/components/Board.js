3603df9327d6787c08c0170e8d99c270e8cd4265
Hue Pham <huepham113@gmail.com>
2020-11-21 00:20:15 -0500

    Add boolean xIsNext to Board constructor; modify handleClick(); modify const status in render() in Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,18 +6,24 @@ import Square from './Square';
 class Board extends Component {
     // 03-lift-state-up: Add a constructor to the Board
     // set the Board’s initial state to contain an array of 9 nulls (corresponding to the 9 squares)
+    // 05-taking-turns: set the first move to be “X” by default
     constructor(props) {
         super(props);
         this.state = {
           squares: Array(9).fill(null),
+          xisNext: true,
         };
     }
 
     // Add function handleClick
+    // 05: Use boolean xIsNext to determine who goes next
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     // In 02-state, X is determined by Square’s own state
@@ -33,7 +39,8 @@ class Board extends Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        // 05: change status to display which player has the next turn
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         
         return (
             <div>
